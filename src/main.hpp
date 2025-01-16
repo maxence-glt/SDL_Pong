8907bfa47f5128c5eaba0b32971ef9ad02d4b1fb
maxence-glt <maxencegilloteaux2023@gmail.com>
2025-01-16 17:07:12 -0500

    added main header file for global game defaults

---
+++ src/main.hpp
@@ -0,0 +1,28 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <string>
+
+#include "ball.hpp"
+#include "paddle.hpp"
+
+const struct GameVars {
+    const std::string name = "SDLTutorial";
+    constexpr static int SCREEN_WIDTH = 1024;
+    constexpr static int SCREEN_HEIGHT = 768;
+
+    const int BALL_X_DEFAULT = SCREEN_WIDTH / 2 - Ball::getWidth() / 2;
+    const int BALL_Y_DEFAULT = SCREEN_HEIGHT / 2 - Ball::getHeight() / 2;
+
+
+    const int RPADDLE_X_DEFAULT = 10;
+    const int LPADDLE_X_DEFAULT = SCREEN_WIDTH - (10 + Paddle::getWidth());
+
+    const int PADDLE_Y_DEFAULT = (SCREEN_HEIGHT / 2) - (Paddle::getHeight() / 2);
+} gameVars;
+
+
+
+
+
+#endif
